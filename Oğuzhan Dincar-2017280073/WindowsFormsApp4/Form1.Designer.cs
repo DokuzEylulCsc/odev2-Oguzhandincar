975ea1fe81c8b86ae7beaf0b38ccd6d1119ece6e
Oguzhandincar <55945703+Oguzhandincar@users.noreply.github.com>
2019-12-01 19:01:44 +0300

    Add files via upload

---
+++ Oğuzhan Dincar-2017280073/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Location = new System.Drawing.Point(107, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dönüştür";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button2.Location = new System.Drawing.Point(164, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dönüştür";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 34);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Location = new System.Drawing.Point(135, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 26);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label2.Location = new System.Drawing.Point(159, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 26);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label3.Location = new System.Drawing.Point(37, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sayı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label4.Location = new System.Drawing.Point(6, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Roma rakamı:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(107, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(184, 34);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Firebrick;
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 257);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Roma rakamına çevirme";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.ForestGreen;
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(335, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(354, 257);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sayıya çevirme";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(702, 280);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Oğuzhan Dincar 2017280073";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Oğuzhan Dincar-2017280073/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = "";
+            int sayi = Convert.ToInt32(textBox3.Text);
+            int binler = sayi / 1000;
+            int yüzler = (sayi % 1000) / 100;
+            int onlar = ((sayi % 1000) % 100) / 10;
+            int birler = ((sayi % 1000) % 100) % 10;
+            if (sayi<4000)
+            {
+                switch (binler)
+                {
+                    case 1: label1.Text = "M"; break;
+                    case 2: label1.Text = "MM"; break;
+                    case 3: label1.Text = "MMM"; break;
+
+                      
+                }
+                switch (yüzler)
+                {
+                    case 1: label1.Text = label1.Text + "C"; break;
+                    case 2: label1.Text = label1.Text + "CC"; break;
+                    case 3: label1.Text = label1.Text + "CCC"; break;
+                    case 4: label1.Text = label1.Text + "CD"; break;
+                    case 5: label1.Text = label1.Text + "D"; break;
+                    case 6: label1.Text = label1.Text + "DC"; break;
+                    case 7: label1.Text = label1.Text + "DCC"; break;
+                    case 8: label1.Text = label1.Text + "DCCC"; break;
+                    case 9: label1.Text = label1.Text + "CM"; break;
+
+                        
+                }
+                switch (onlar)
+                {
+                    case 1: label1.Text = label1.Text + "X"; break;
+                    case 2: label1.Text = label1.Text + "XX"; break;
+                    case 3: label1.Text = label1.Text + "XXX"; break;
+                    case 4: label1.Text = label1.Text + "XL"; break;
+                    case 5: label1.Text = label1.Text + "L"; break;
+                    case 6: label1.Text = label1.Text + "LX"; break;
+                    case 7: label1.Text = label1.Text + "LXX"; break;
+                    case 8: label1.Text = label1.Text + "LXXX"; break;
+                    case 9: label1.Text = label1.Text + "XC"; break;
+
+                }
+                switch (birler)
+                {
+                    case 1: label1.Text = label1.Text + "I"; break;
+                    case 2: label1.Text = label1.Text + "II"; break;
+                    case 3: label1.Text = label1.Text + "III"; break;
+                    case 4: label1.Text = label1.Text + "IV"; break;
+                    case 5: label1.Text = label1.Text + "V"; break;
+                    case 6: label1.Text = label1.Text + "VI"; break;
+                    case 7: label1.Text = label1.Text + "VII"; break;
+                    case 8: label1.Text = label1.Text + "VIII"; break;
+                    case 9: label1.Text = label1.Text + "IX"; break;
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("ABİM YANLIŞ DEĞER GİRDİN(1-4000 GİRİLEBİLİR.)");
+            }
+
+
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            String rakam = textBox2.Text.ToUpper();  //Küçük harfleri kabul etmesi için.
+
+            int sayi = 0, a1 = 0, a2 = 0, a3 = 0;
+            for (int i = 0; i < rakam.Length; i++)
+            {
+                a2 = a1;
+                switch (rakam[i])
+                {
+                    case 'I':
+                        a1 = 1;
+                        break;
+                    case 'V':
+                        a1 = 5;
+                        break;
+                    case 'X':
+                        a1 = 10;
+                        break;
+                    case 'L':
+                        a1 = 50;
+                        break;
+                    case 'C':
+                        a1 = 100;
+                        break;
+                    case 'D':
+                        a1 = 500;
+                        break;
+                    case 'M':
+                        a1 = 1000;
+                        break;
+                    default: MessageBox.Show("ABİM HATA YAPTIN");
+                        i = 100;   //Döngüden çıkmak için.
+                        break;
+                        
+
+                }
+               
+                if (a1 > a2)
+                {
+                    a3 = -2 * a2;
+                }
+                else
+                {
+                    a3 = 0;
+                }
+                sayi += a1+a3;
+
+                if (sayi<4000)
+                {
+                    label2.Text = sayi.ToString();
+                    textBox2.Clear();
+
+                }
+                else
+                {
+                    label2.Text = "";
+                    MessageBox.Show("ABİM 4000'DEN YUKARISI KURTARMAZ");
+                    break;
+                }
+            }
+            
+            
+        
+            
+        }
+
+        private void textBox3_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+                 && !char.IsSeparator(e.KeyChar);
+        }
+    }
+    
+}
+
